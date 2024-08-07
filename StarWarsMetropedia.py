03687637aaa7a47ac292432bcd808a246d8339e1
Arianna <arianna.arenare@correo.unimet.edu.ve>
2024-08-07 00:35:32 -0400

    Diseño de menú

--- StarWarsMetropedia.py
+++ StarWarsMetropedia.py
@@ -1,4 +1,43 @@
 
 class StarWarsMetropedia:
     def start(self):
-        None
+        print("Sea bienvenido a la Metropedia de Star Wars")
+        while True:
+            menu=input("""
+Ingrese una opción: 
+1- Lista de películas
+2- Lista de especies de seres vivos de la saga 
+3- Lista de planetas
+4-Buscar personaje
+5- Ver gráfico de cantidad de personajes nacidos en cada planeta 
+6- Naves
+7- Misiones
+8- Volver al menú
+--->""")
+            if menu=="1":
+                None
+
+            elif menu=="2":
+                None
+
+            elif menu=="3":
+                None
+
+            elif menu=="4":
+                None
+
+            elif menu=="5":
+                None
+
+            elif menu=="6":
+                None
+
+            elif menu=="7":
+                None
+
+            elif menu=="8":
+                continue
+
+            elif menu=="9":
+                break
+            
