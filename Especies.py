72ee3677f102c8f5ae86508d97f5e1a23960b863
Jose Antonio Padron <jose-padron@correo.unimet.edu.ve>
2024-08-07 16:54:41 -0400

    Add files via upload
    Creacion de clases

---
+++ Especies.py
@@ -0,0 +1,14 @@
+class Especies: #Transformacion de los datos a la clase de especie
+    def __init__(self,nombre,altura,clasificacion,planeta,lengua_materna,nombre_de_los_personajes_de_especie,nombre_de_episodios):
+        self.nombre=nombre
+        self.altura=altura
+        self.clasificacion=clasificacion
+        self.planeta=planeta
+        self.lengua_materna=lengua_materna
+        self.nombre_de_los_personajes_de_especie=nombre_de_los_personajes_de_especie
+        self.nombre_de_episodios=nombre_de_episodios
+        
+
+        
+        
+        
