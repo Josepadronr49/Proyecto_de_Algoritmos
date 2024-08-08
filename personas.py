24063e94c544d8ab76769b2d8e8db54eb24abccb
Arianna <arianna.arenare@correo.unimet.edu.ve>
2024-08-08 01:32:00 -0400

    Prueba código personajes

--- personas.py
+++ personas.py
@@ -13,7 +13,8 @@ def buscar_personaje():
         caracteristicas_personajes=personaje["url"]
         caracteristicas_personajes_db=initial_data_base(caracteristicas_personajes)
         caracteristicas=caracteristicas_personajes_db["results"]
-        personaje_obj.append(Personaje(caracteristicas["name"],caracteristicas["name"],caracteristicas["name"],caracteristicas["name"],caracteristicas["name"],caracteristicas["name"],caracteristicas["name"]))
+        personaje_obj.append(Personaje(caracteristicas["name"],initial_data_base(caracteristicas["url"])["properties"]["name"],"titulo episodio",caracteristicas["gender"],"especie","nave","vehiculos"))
+        buscar_personaje=input("Ingrese el nombre del personaje que desee buscar: ")
         #print(personaje_caracteristicas_db)
         #print(personaje_caracteristica)
         #personaje_caracteristica=initial_data_base("https://www.swapi.tech/api/people/uid")
