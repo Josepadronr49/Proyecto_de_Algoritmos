cb624583b011741995f76d394a4a95ce27c1141e
Arianna <arianna.arenare@correo.unimet.edu.ve>
2024-08-07 21:25:03 -0400

    Parte A del menú: Lista de películas

--- StarWarsMetropedia.py
+++ StarWarsMetropedia.py
@@ -1,8 +1,12 @@
+from Funciones import cargar_API
+from Pelicula import Pelicula
 
 #Se crea una clase en la que va a estar el código del menú del programa
 class StarWarsMetropedia:
+    pelicula_obj=[]
+
     def start(self):
-        print("Sea bienvenido a la Metropedia de Star Wars")
+        print("¡Sea bienvenido a la Metropedia de Star Wars!")
         while True:
             menu=input("""
 Ingrese una opción: 
@@ -14,9 +18,10 @@ Ingrese una opción:
 6- Naves
 7- Misiones
 8- Volver al menú
+9-Salir
 --->""")
             if menu=="1":
-                None
+                self.mostrar_lista_peliculas()
 
             elif menu=="2":
                 None
@@ -41,4 +46,17 @@ Ingrese una opción:
 
             elif menu=="9":
                 break
-            
+        print("¡Hasta luego!")
+
+
+#Se crea una función para la primera opción del menú de la lista de planetas
+    def mostrar_lista_peliculas(self):
+        db_peliculas=cargar_API("https://www.swapi.tech/api/films")
+        peliculas=db_peliculas["result"]
+        for pelicula in peliculas:
+            pelicula_propiedades=pelicula["properties"]
+            self.pelicula_obj.append(Pelicula(pelicula_propiedades["title"],pelicula_propiedades["episode_id"],pelicula_propiedades["release_date"],pelicula_propiedades["opening_crawl"],pelicula_propiedades["director"]))
+        for peli in self.pelicula_obj:
+            peli.mostrar_pelicula()
+
+      
