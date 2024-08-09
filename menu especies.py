74ff53e27b0515fafd54fb789f5552faf01d606d
Jose Antonio Padron <jose-padron@correo.unimet.edu.ve>
2024-08-09 16:27:04 -0400

    Add files via upload
    Actualizacion de archivos

--- menu especies.py
+++ menu especies.py
@@ -1,11 +1,20 @@
-from Funciones import cargar_API
+import requests as rq
+from Funciones import cargar_API,cargar_informacion
 from Especies import Especies
 especies_obj=[]
-dict_especies=cargar_API('https://www.swapi.tech/api/species?page=2&limit=10000')
-for propiedades_especies in dict_especies['results']:
-    propiedad=cargar_API(propiedades_especies['url'])
-    dict_propiedades=propiedad['result']['properties']
-    especies_obj.append(Especies(dict_propiedades['name'],dict_propiedades['average_height'],dict_propiedades['classification'],cargar_API(str(dict_propiedades['homeworld']))['result']['properties']['name'],dict_propiedades['language'],dict_propiedades['people'],'pelicula')) #hay que arreglar lista de personas y pelicula
+
+
+db_especies=cargar_informacion('https://swapi.dev/api/species/?format=json')
+#print(cargar_API(None))
+#print(db_especies)
+
+for especies in db_especies:
+    for especie in especies:
+        lista_personajes=[]
+        for personaje in especie["people"]:
+            cada_personaje=cargar_API(personaje)
+            lista_personajes.append(cada_personaje["name"])     
+        especies_obj.append(Especies(especie['name'],especie['average_height'],especie['classification'],cargar_API(especie["homeworld"])['name'],especie['language'],lista_personajes,'pelicula')) #hay que arreglar lista de personas y pelicula
     
 for clase_planeta in especies_obj:
     clase_planeta.mostrar_especies()
