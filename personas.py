72bacf67f4854bee9951d4fef84746756fa88722
Arianna <arianna.arenare@correo.unimet.edu.ve>
2024-08-08 21:27:11 -0400

    Se agregó función de convertir personajes a objetos

--- personas.py
+++ personas.py
@@ -21,8 +21,23 @@ def cargar_informacion(link):
 personaje_informacion=cargar_informacion("https://swapi.dev/api/people/?format=json")
 for personajes in personaje_informacion:
     for personaje in personajes:
-        personaje_obj.append(Personaje(personaje["name"],personaje["name"],personaje["name"],personaje["name"],personaje["name"],personaje["name"],personaje["name"]))
-
+        lista_peliculas=[]
+        for pelicula in personaje["films"]:
+            cada_pelicula=initial_data_base(pelicula)
+            lista_peliculas.append(cada_pelicula["title"])
+        lista_especies=[]
+        for especie in personaje["species"]:
+            cada_especie=initial_data_base(especie)
+            lista_especies.append(cada_especie["name"])
+        lista_naves=[]
+        for nave in personaje["starships"]:
+            cada_nave=initial_data_base(nave)
+            lista_naves.append(cada_nave["name"])
+        lista_vehiculos=[]
+        for vehiculo in personaje["vehicles"]:
+            cada_vehiculo=initial_data_base(vehiculo)
+            lista_vehiculos.append(cada_vehiculo["name"])
+        personaje_obj.append(Personaje(personaje["name"],initial_data_base(personaje["homeworld"])["name"],lista_peliculas,personaje["gender"],lista_especies,lista_naves,lista_vehiculos))
 
 
 
