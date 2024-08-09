ff1a225152434c82414aad74053571c3e28fb606
Jose Antonio Padron <jose-padron@correo.unimet.edu.ve>
2024-08-09 17:34:57 -0400

    Actualizacion de cargar_api

--- Funciones.py
+++ Funciones.py
@@ -1,7 +1,7 @@
 import requests as rq
 def cargar_API(link):
-    if link==None:
-        informacion='Desconocido'
+    if link==None: #Solucion a los links vacios del api, para que devuelva un diccionario iterable.
+        informacion={'name':'Desconocido','title':'Desconocido'}
     else:informacion=rq.get(link).json()
     return informacion
 
--- menu especies.py
+++ menu especies.py
@@ -5,16 +5,18 @@ especies_obj=[]
 
 
 db_especies=cargar_informacion('https://swapi.dev/api/species/?format=json')
-#print(cargar_API(None))
-#print(db_especies)
+
 
 for especies in db_especies:
     for especie in especies:
         lista_personajes=[]
+        lista_peliculas=[]
         for personaje in especie["people"]:
             cada_personaje=cargar_API(personaje)
-            lista_personajes.append(cada_personaje["name"])     
-        especies_obj.append(Especies(especie['name'],especie['average_height'],especie['classification'],cargar_API(especie["homeworld"])['name'],especie['language'],lista_personajes,'pelicula')) #hay que arreglar lista de personas y pelicula
-    
+            lista_personajes.append(cada_personaje["name"])
+        for peliculas in especie['films']:
+            cada_pelicula=cargar_API(peliculas)
+            lista_peliculas.append(cada_pelicula['title'])      
+        especies_obj.append(Especies(especie['name'],especie['average_height'],especie['classification'],cargar_API(especie["homeworld"])['name'],especie['language'],lista_personajes,lista_peliculas)) #Conversion de los objetos a clase Especie
 for clase_planeta in especies_obj:
     clase_planeta.mostrar_especies()
