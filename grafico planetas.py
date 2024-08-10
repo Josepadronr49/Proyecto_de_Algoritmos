79f85372968228b08ecfb252a54a35637c13e7c9
Arianna <arianna.arenare@correo.unimet.edu.ve>
2024-08-09 23:20:45 -0400

    Últimas modificaciones al archivo de graficos de personajes por planeta

--- grafico planetas.py
+++ grafico planetas.py
@@ -2,18 +2,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Se accede al archivo utilizando la librería pandas
 db_archivo_planeta = pd.read_csv("csv/planets.csv")
 
-db_archivo_planeta["residents"]=db_archivo_planeta["residents"].fillna("")
-db_archivo_planeta["residents"]=db_archivo_planeta["residents"].apply(lambda x: len(x.split(",")) if x else 0)
-print(db_archivo_planeta[["name","residents"]].head())
+db_archivo_planeta["residents"]=db_archivo_planeta["residents"].fillna("") #Reemplaza el NaN por cadena vacía
+db_archivo_planeta["residents"]=db_archivo_planeta["residents"].apply(lambda x: len(x.split(",")) if x else 0) #Cuenta a los residentes
+print(db_archivo_planeta[["name","residents"]].head()) #Se verifican los datos
 
+#Con las funciones de la librería marplotlib se puede crear un gráfico 
+#Se escoge un gráfico de barras para que la comparación sea más cómoda a nivel visual 
 plt.figure(figsize=(12,8))
 plt.bar(db_archivo_planeta["name"],db_archivo_planeta["residents"],color=["deeppink","hotpink","fuchsia","violet"])
-plt.title("Cantidad de personajes nacidos en cada planeta")
-plt.xlabel("Planetas")
-plt.ylabel("Cantidad de personajes nacidos")
+plt.title("Cantidad de personajes nacidos en cada planeta") #Se escoge el título del gráfico
+plt.xlabel("Planetas") #Leyenda del eje "x"
+plt.ylabel("Cantidad de personajes nacidos") #Leyenda del eje "y"
 plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+plt.tight_layout()  
+plt.show()  #Se muestra el gráfico
 
